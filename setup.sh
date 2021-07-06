8a16a7646c3877ed86a18840968cff6a3a966032
SmallPepperZ <69822582+SmallPepperZ@users.noreply.github.com>
2021-07-06 13:35:03 -0700

    add a requirements file

--- setup.sh
+++ setup.sh
@@ -6,3 +6,7 @@ if ! [ -f settings.cfg ]
 then
 	cp settings.cfg.example settings.cfg
 fi
+cd $(dirname "$0")
+python3 -m venv venv
+source ./venv/bin/activate
+python3 -m pip install -r requirements.txt
